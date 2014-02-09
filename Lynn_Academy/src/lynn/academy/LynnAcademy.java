7660efefcfb883acdf505ba2d7f27beb392c76f4
AlexKimani <alexk249076@gmail.com>
2014-02-09 12:00:48 +0300

    Initial

---
+++ Lynn_Academy/src/lynn/academy/LynnAcademy.java
@@ -0,0 +1,15 @@
+
+package lynn.academy;
+import javax.swing.JFrame;
+import javax.swing.event.*;
+import java.awt.*;
+public class LynnAcademy extends JFrame{
+
+    public LynnAcademy(){
+        
+    }
+
+    public static void main(String[] args) {
+   
+    }
+}
